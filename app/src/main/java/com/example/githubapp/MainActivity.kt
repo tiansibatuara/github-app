765a2a61dbd9fdf39411a3e01573d34cdab327d1
tiansibatuara <sibatuarachristian@gmail.com>
2023-03-16 18:50:35 +0700

    update

--- app/src/main/java/com/example/githubapp/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.githubapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.githubapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
     }
 }
---
+++ app/src/main/java/com/example/githubapp/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.githubapp
+
+class MainViewModel {
+}
